01df6d29fe1cbdb31de6ea9519adae12befb6323
Raul.Rivas <Raul.Rivas@antechimagingservices.com>
2025-07-29 15:59:25 -0400

    first commit

---
+++ backend/src/index.js
@@ -0,0 +1,12 @@
+import express from "express";
+
+import authRoutes from './routes/auth.route.js';
+
+const  app = express();
+
+app.use("/api/auth", authRoutes)
+
+app.listen(5001 , ()=>{
+    console.log("The server is running on the port 5001 ");
+})
+
